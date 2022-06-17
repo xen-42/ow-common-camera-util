cf5a23ce59749aae89b0bfba6f371b59df0aae87
Nick <nicholasconnors@hotmail.ca>
2022-06-17 17:15:26 -0400

    I have no idea how IK works

--- CommonCameraUtil/Components/RoastingStickAnim.cs
+++ CommonCameraUtil/Components/RoastingStickAnim.cs
@@ -15,14 +15,20 @@ namespace CommonCameraUtil.Components
 
         private Transform _shoulder, _elbow, _hand;
 
-        private Quaternion _elbowBaseRot;
-        private Vector3 _handOffset;
+        private Quaternion _elbowBaseRot, _shoulderBaseRot;
+        private Vector3 _handOffset, _elbowOffset;
 
         private RoastingStickController _controller;
 
+        private float _armLength;
+
+        public Vector3 offset;
+
+        private GameObject _debugShape;
+
         private void Awake()
         {
-            _playerAnimator = Locator.GetPlayerBody().transform.Find("Traveller_HEA_Player_v2").GetComponent<Animator>();
+            _playerAnimator = gameObject.transform.Find("Traveller_HEA_Player_v2").GetComponent<Animator>();
 
             _roastingStick = Util.Find("Player_Body/RoastingSystem/Stick_Root/Stick_Pivot/Stick_Tip").transform;
 
@@ -31,12 +37,25 @@ namespace CommonCameraUtil.Components
             _shoulder = _playerAnimator.GetBoneTransform(HumanBodyBones.RightUpperArm);
 
             _elbowBaseRot = _elbow.localRotation;
+            _shoulderBaseRot = _shoulder.localRotation;
 
             _handOffset = _elbow.InverseTransformPoint(_hand.position);
+            _elbowOffset = _shoulder.InverseTransformPoint(_elbow.position);
 
-            _controller = Locator.GetPlayerBody().GetComponentInChildren<RoastingStickController>();
+            _controller = gameObject.GetComponentInChildren<RoastingStickController>();
+
+
 
             GlobalMessenger<OWCamera>.AddListener("SwitchActiveCamera", OnSwitchActiveCamera);
+
+            _debugShape = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            _debugShape.transform.localScale = Vector3.one * 0.1f;
+            GameObject.Destroy(_debugShape.GetComponent<Collider>());
+        }
+
+        private void Start()
+        {
+            _armLength = (_hand.position - _elbow.position).magnitude;
         }
 
         private void OnDestroy()
@@ -49,9 +68,9 @@ namespace CommonCameraUtil.Components
             if (CommonCameraUtil.UsingCustomCamera())
             {
                 _controller._stickMinZ = 1.5f;
-                _controller._stickMaxZ = 2.5f;
+                _controller._stickMaxZ = 2.1f;
 
-                _controller.transform.position = _elbow.position;
+                _controller.transform.localPosition = new Vector3(0.3f, 0.15f, 0.04f);
             }
             else
             {
@@ -67,6 +86,19 @@ namespace CommonCameraUtil.Components
             if (!IsRoasting || !CommonCameraUtil.UsingCustomCamera()) return;
 
             _elbow.transform.localRotation = Quaternion.FromToRotation(_handOffset.normalized, _elbow.InverseTransformPoint(_roastingStick.position).normalized) * _elbowBaseRot;
+
+            // Closest distance from elbow to stick
+            var closest = Util.ProjectPointLine(_elbow.position, _roastingStick.position, _roastingStick.position - _roastingStick.forward * 4f);
+
+            var handle = closest + _roastingStick.forward * _armLength + _roastingStick.TransformVector(offset);
+
+            var elbowPos = handle + (_elbow.position - handle).normalized * _armLength;
+
+            _shoulder.transform.localRotation = Quaternion.FromToRotation(_elbowOffset.normalized, _shoulder.InverseTransformPoint(elbowPos).normalized) * _shoulderBaseRot;
+            _elbow.transform.position = elbowPos;
+            _hand.transform.position = handle;
+
+            _debugShape.transform.position = handle;
 
             var head = _playerAnimator.GetBoneTransform(HumanBodyBones.Head);
             head.LookAt(_roastingStick, _playerAnimator.transform.up);
--- CommonCameraUtil/Util.cs
+++ CommonCameraUtil/Util.cs
@@ -70,5 +70,24 @@ namespace CommonCameraUtil
 
             return go;
         }
+
+        public static float DistancePointLine(Vector3 point, Vector3 lineStart, Vector3 lineEnd)
+        {
+            return Vector3.Magnitude(ProjectPointLine(point, lineStart, lineEnd) - point);
+        }
+
+        public static Vector3 ProjectPointLine(Vector3 point, Vector3 lineStart, Vector3 lineEnd)
+        {
+            Vector3 rhs = point - lineStart;
+            Vector3 vector2 = lineEnd - lineStart;
+            float magnitude = vector2.magnitude;
+            Vector3 lhs = vector2;
+            if (magnitude > 1E-06f)
+            {
+                lhs = (Vector3)(lhs / magnitude);
+            }
+            float num2 = Mathf.Clamp(Vector3.Dot(lhs, rhs), 0f, magnitude);
+            return (lineStart + ((Vector3)(lhs * num2)));
+        }
     }
 }
